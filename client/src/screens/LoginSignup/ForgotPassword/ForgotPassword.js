abac8065f1e4a8a7362fac3f7ce024ddc848d68d
K.Thoyajaksha Kashyap <kristipatithoyajakshakashyap@gmail.com>
2023-07-20 19:50:12 +0530

    test ok

--- client/src/screens/LoginSignup/ForgotPassword/ForgotPassword.js
+++ client/src/screens/LoginSignup/ForgotPassword/ForgotPassword.js
@@ -4,7 +4,7 @@ import React from 'react'
 const ForgotPassword = () => {
   return (
     <View>
-      <Text>ForgotPassword inital commit</Text>
+      <Text>ForgotPassword</Text>
     </View>
   )
 }
